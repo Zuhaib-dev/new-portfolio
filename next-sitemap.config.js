f9cd433809b744aaf2bbaf6332b1bb9769dded46
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-27 14:47:54 +0530

    Refactor sitemap configuration and update package dependencies

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,24 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://zuhaibrashid.com/</loc>
-    <lastmod>2025-09-28</lastmod>
-    <priority>1.0</priority>
-  </url>
-</urlset>
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.zuhaibrashid.com',
+  generateRobotsTxt: true,          // creates robots.txt automatically
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  // add the resume PDF explicitly so it appears in the sitemap
+  additionalPaths: async (config) => {
+    const paths = [];
+    // use config.transform so next-sitemap formats the entry correctly
+    paths.push(await config.transform(config, '/Zuhaib%20Rashid.pdf', 'monthly', 0.8));
+    return paths;
+  },
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' }
+    ],
+    // optional: make sure robots.txt references the sitemap
+    additionalSitemaps: [
+      'https://www.zuhaibrashid.com/sitemap.xml'
+    ]
+  }
+}
