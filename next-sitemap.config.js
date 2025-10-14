02f959b9c30729c1770432bc6a6416da0238e177
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-10-14 21:40:07 +0530

    updated the next-sitemap.config.js cause i was facing some errors in search console

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,24 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.zuhaibrashid.com',
-  generateRobotsTxt: true,          // creates robots.txt automatically
+  generateRobotsTxt: true,
   changefreq: 'monthly',
   priority: 0.7,
   sitemapSize: 5000,
-  // add the resume PDF explicitly so it appears in the sitemap
   additionalPaths: async (config) => {
     const paths = [];
-    // use config.transform so next-sitemap formats the entry correctly
     paths.push(await config.transform(config, '/Zuhaib%20Rashid.pdf', 'monthly', 0.8));
     return paths;
   },
   robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' }
-    ],
-    // optional: make sure robots.txt references the sitemap
-    additionalSitemaps: [
-      'https://www.zuhaibrashid.com/sitemap.xml'
-    ]
-  }
-}
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
