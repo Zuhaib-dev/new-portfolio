e8389b8d8fe3b38e5fbdcb7095d737bf8dfa2903
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-07-26 14:03:20 +0530

    feat: integrate Google Tag Manager and add sitemap files for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -216,6 +216,16 @@ export default function RootLayout({
       </head>
 
       <body className={`${inter.className} min-h-screen relative overflow-x-hidden`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2WHDB77"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {/* Global Background Effects */}
         <div className="fixed inset-0 bg-noise z-[-1]" />
         <div className="fixed inset-0 bg-glow z-[-1]" />
@@ -259,6 +269,19 @@ export default function RootLayout({
             </SmoothScroll>
           </Suspense>
         </ThemeProvider>
+
+        {/* ✅ Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W2WHDB77');`,
+          }}
+        />
 
         {/* ✅ Microsoft Clarity */}
         <Script
